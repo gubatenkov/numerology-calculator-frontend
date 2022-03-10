07cc0c913848d9477df39de1a32e035b769f575e
s9379992g <s9379992g@gmail.com>
2022-03-10 15:24:18 +0200

    improved ui of form on register page; added form validation;

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -23,3 +23,5 @@ export const LANGUAGES = [
 export const DEFAULT_LANGUAGE_ID = "de";
 
 export const LANGUAGE_KEY = "lang";
+
+export const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
---
+++ src/utils/useValidators.jsx
@@ -0,0 +1,25 @@
+import { EMAIL_REGEX } from "./Constants";
+import { useTranslation } from "react-i18next";
+
+const useValidators = () => {
+  const { t } = useTranslation();
+
+  const emailValidators = {
+    required: t("FIELD_REQUIRED"),
+    maxLength: { value: 128, message: t("EMAIL_CANT_BE_MORE_128") },
+    pattern: { value: EMAIL_REGEX, message: t("ENTER_EXISTING_EMAIL") }
+  };
+
+  const passwordValidators = {
+    required: t("FIELD_REQUIRED"),
+    maxLength: {
+      value: 128,
+      message: t("PASS_CANT_BE_MORE_128")
+    },
+    minLength: { value: 8, message: t("PASS_CANT_BE_LESS_8") }
+  };
+
+  return [emailValidators, passwordValidators];
+};
+
+export default useValidators;
